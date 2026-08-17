bae928ec8586e8eb72d2966c458c5d651237295b
rampoz27 <swordmiracle@gmail.com>
2026-08-17 13:18:52 +0700

    Create page.tsx

---
+++ src/app/learning/page.tsx
@@ -0,0 +1,9 @@
+import LearningWorkspace from './components/LearningWorkspace';
+
+export default function LearningPage() {
+  return (
+    <div className="flex h-full overflow-hidden">
+      <LearningWorkspace />
+    </div>
+  );
+}
